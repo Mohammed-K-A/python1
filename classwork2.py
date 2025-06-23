109887b7896a8c625c1fc41bc1f567b87f99c262
unknown <iammohammedka@gmail.com>
2025-06-23 20:40:45 +0530

    new work updated

---
+++ classwork2.py
@@ -0,0 +1,36 @@
+# Qn: First unique character in a string
+# Given a string s, find its first non-repeating character and return its index. If it does not exist, return -1.
+
+# string = input("Enter a string: ")
+# if 1 <= len(string) <= 10**5:
+#     for i in range(len(string)):
+#         count=string.count(string[i])
+#         if count == 1:
+#             print(i)
+#             break
+#     else:
+#         print("-1")
+# else:
+#     print("Enter a valid length")
+
+
+
+
+# Qn. First missing positive number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ generator.py
@@ -0,0 +1,26 @@
+# example 1
+
+# def gener1():
+#     yield 10
+#     yield 20
+#     yield 30
+#     yield 40
+# num=gener1()
+# print(next(num))                          #one by one aayi print cheyyum(1st 10 , pinne 20 angane.......)
+# print(next(num))
+# print(next(num))
+# print(next(num))
+# print(next(num))
+
+
+# example 2
+
+# def gener1():
+#     yield 10
+#     yield 20
+#     yield 30
+#     yield 40
+#     yield 50
+# num=gener1()    
+# for i in num:                              #mothathil print cheyyum 10-50
+#     print(i)
